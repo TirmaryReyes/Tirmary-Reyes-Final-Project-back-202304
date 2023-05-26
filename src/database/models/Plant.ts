f403d0b4d7ae86e1242314a9b83d3511d3a05cfb
Tirmary Reyes <tmrv1991@gmail.com>
2023-05-26 22:48:55 +0200

    Add plant database model

---
+++ src/database/models/Plant.ts
@@ -0,0 +1,33 @@
+import { Schema, Types, model } from "mongoose";
+import User from "./User.js";
+
+const plantSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  size: {
+    type: String,
+    required: true,
+  },
+  hasFlowers: {
+    type: Boolean,
+    required: true,
+  },
+  user: {
+    type: Types.ObjectId,
+    ref: User,
+  },
+});
+
+const Plant = model("Plant", plantSchema, "plants");
+
+export default Plant;
