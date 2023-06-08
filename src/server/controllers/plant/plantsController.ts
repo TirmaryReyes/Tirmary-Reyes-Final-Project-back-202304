1210775aeddb8c52b546e073bf5be8298c672694
Tirmary Reyes <tmrv1991@gmail.com>
2023-06-08 18:04:24 +0200

    Add delete controller plant

--- src/server/controllers/plant/plantsController.ts
+++ src/server/controllers/plant/plantsController.ts
@@ -2,12 +2,18 @@ import "../../../loadEnvironment.js";
 import createDebug from "debug";
 import { type NextFunction, type Request, type Response } from "express";
 import Plant from "../../../database/models/Plant.js";
+import CustomError from "../../../CustomError/CustomError.js";
+import { type CustomRequest } from "../../types.js";
 
 const debug = createDebug(
   "sandRose-api:server:controllers:plant:plantsController.js"
 );
 
-const getPlants = async (req: Request, res: Response, next: NextFunction) => {
+export const getPlants = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const plants = await Plant.find().limit(10).exec();
     res.status(200).json({ plants });
@@ -18,4 +24,24 @@ const getPlants = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
-export default getPlants;
+export const deletePlant = async (
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { idPlant } = req.params;
+
+    const plantRemoved = await Plant.findByIdAndDelete(idPlant).exec();
+
+    if (!plantRemoved) {
+      const error = new CustomError(404, "Plant not found");
+
+      throw error;
+    }
+
+    res.status(200).json({ message: "Plant removed" });
+  } catch (error: unknown) {
+    next(error);
+  }
+};
--- src/server/routers/user/plants/plantsRouter.ts
+++ src/server/routers/user/plants/plantsRouter.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
-import getPlants from "../../../controllers/plant/plantsController.js";
+import {
+  deletePlant,
+  getPlants,
+} from "../../../controllers/plant/plantsController.js";
+import { auth } from "../../../middlewares/authMiddleware/authMiddleware.js";
 
 const plantsRouter = Router();
 
 plantsRouter.get("/", getPlants);
+plantsRouter.delete("/:idPlant", auth, deletePlant);
 
 export default plantsRouter;
