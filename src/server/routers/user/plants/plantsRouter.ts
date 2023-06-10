3ef195bd8d3312bfade9738ca774efb665fd6953
Tirmary Reyes <tmrv1991@gmail.com>
2023-06-10 21:51:58 +0200

    Add plantRouter post endpoint

--- src/server/routers/user/plants/plantsRouter.ts
+++ src/server/routers/user/plants/plantsRouter.ts
@@ -1,13 +1,20 @@
 import { Router } from "express";
 import {
+  addPlants,
   deletePlant,
   getPlants,
 } from "../../../controllers/plant/plantsController.js";
-import { auth } from "../../../middlewares/authMiddleware/authMiddleware.js";
+import { validate } from "express-validation";
+import { createPlantSchema } from "../../../utils/schemas/plantSchema.js";
 
 const plantsRouter = Router();
 
 plantsRouter.get("/", getPlants);
-plantsRouter.delete("/:idPlant", auth, deletePlant);
+plantsRouter.delete("/:idPlant", deletePlant);
+plantsRouter.post(
+  "/add",
+  validate(createPlantSchema, {}, { abortEarly: false }),
+  addPlants
+);
 
 export default plantsRouter;
